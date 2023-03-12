66b82bddb7067d26b668ef8b69e0693c494d9e00
AlejandroAisa36 <alexaisa1999@gmail.com>
2023-03-12 17:01:21 +0100

    "" instead of ''

--- Followers_function.R
+++ Followers_function.R
@@ -1,15 +1,15 @@
 # Listas de años: 
 
-tens = '37i9dQZF1DX5Ejj0EkURtP' 
-zeros = '37i9dQZF1DX4o1oenSJRJd'
-nineties = '37i9dQZF1DXbTxeAdrVG2l' 
-eighties = '37i9dQZF1DX4UtSsGT1Sbe'
-seventies = '37i9dQZF1DWTJ7xPn4vNaz' 
-sixties = '37i9dQZF1DXaKIA8E7WcJj'
+tens = "37i9dQZF1DX5Ejj0EkURtP" 
+zeros = "37i9dQZF1DX4o1oenSJRJd"
+nineties = "37i9dQZF1DXbTxeAdrVG2l" 
+eighties = "37i9dQZF1DX4UtSsGT1Sbe"
+seventies = "37i9dQZF1DWTJ7xPn4vNaz" 
+sixties = "37i9dQZF1DXaKIA8E7WcJj"
 
 # Playlists 
 
-API_playlist = 'https://api.spotify.com/v1/playlists/'
+API_playlist = "https://api.spotify.com/v1/playlists/"
 years = c(sixties, seventies, eighties, nineties, zeros, tens)
 
 # The function 
@@ -28,18 +28,18 @@ followers = function(x) {
   resp_followers = 
     resp_output %>% 
     enframe() %>% 
-    filter(name == 'followers') %>% 
+    filter(name == "followers") %>% 
     unnest(cols = value) %>% 
-    filter(!value == 'NULL') %>% 
+    filter(!value == "NULL") %>% 
     select(-name) %>% 
-    rename('followers' = 'value')
+    rename("followers" = "value")
   
   resp_name = resp_output %>% 
     enframe() %>% 
-    filter(name == 'name') %>% 
+    filter(name == "name") %>% 
     unnest(cols = value) %>% 
     select(-name) %>% 
-    rename('list' = 'value')
+    rename("list" = "value")
     
   df = cbind(resp_name, resp_followers)
   }
